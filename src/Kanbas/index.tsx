4b8ce719c0daa6f6057a38f2a16755abd96162b3
NoXX-trdmk <davidlion8@gmail.com>
2024-04-17 23:52:54 -0400

    a6 Mongo su23

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -9,6 +9,7 @@ import store from "./store";
 import { Provider } from "react-redux";
 import axios from "axios";
 const API_BASE = process.env.REACT_APP_API_BASE;
+const COURSES_API = "http://localhost:4000/api/courses";
 
 function Kanbas() {
   const [courses, setCourses] = useState<any[]>([]);
