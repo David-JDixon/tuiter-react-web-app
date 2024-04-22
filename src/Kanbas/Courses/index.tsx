9687b317dab6fff26a317527effa0af51519230e
David-JDixon <98180004+David-JDixon@users.noreply.github.com>
2024-04-21 20:31:10 -0400

    Add files via upload

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,4 +1,4 @@
-//import { courses } from "../../Kanbas/Database";
+import { courses } from "../../Kanbas/Database";
 import { Navigate, Route, Routes, useParams } from "react-router-dom";
 import { HiMiniBars3 } from "react-icons/hi2";
 import CourseNavigation from "./Navigation";
@@ -8,7 +8,8 @@ import Assignments from "./Assignments";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-function Courses({ courses }: { courses: any[]; }) {
+
+function Courses() {
   const { courseId } = useParams();
   const COURSES_API = "http://localhost:4000/api/courses";
   const [course, setCourse] = useState<any>({ _id: "" });
@@ -24,24 +25,26 @@ function Courses({ courses }: { courses: any[]; }) {
 
 
   return (
-    <div className="container">
-      <div className="row"><h1><HiMiniBars3 /> Course {course?.name} </h1></div>
-      <div className="row">
-      <div className="col-lg-3"><CourseNavigation /></div>
-      <div className="col-lg-9">
+    <div>
+      <h1><HiMiniBars3 /> Course {course?.name}</h1>
+      <CourseNavigation />
+      <div>
+        <div
+          className="overflow-y-scroll position-fixed bottom-0 end-0"
+          style={{ left: "320px", top: "50px" }} >
           <Routes>
             <Route path="/" element={<Navigate to="Home" />} />
-            <Route path="Home" element={<Home />} />
+            <Route path="Home" element={<Home/>} />
             <Route path="Modules" element={<Modules/>} />
-            <Route path="Assignments"
-                   element={<Assignments/>} />
-            <Route path="Assignments/:assignmentId"
-                   element={<h1>Assignment Editor</h1>}/>
+            <Route path="Piazza" element={<h1>Piazza</h1>} />
+            <Route path="Assignments" element={<Assignments/>} />
+            <Route path="Assignments/:assignmentId" element={<h1>Assignment Editor</h1>} />
             <Route path="Grades" element={<h1>Grades</h1>} />
           </Routes>
         </div>
       </div>
     </div>
-);}
+  );
+}
 
 export default Courses;
