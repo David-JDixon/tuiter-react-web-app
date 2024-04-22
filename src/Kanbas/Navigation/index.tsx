c40a6251874ed264c86ca27da1d3a52ae473594a
David-JDixon <98180004+David-JDixon@users.noreply.github.com>
2024-04-21 20:33:46 -0400

    Add files via upload

--- src/Kanbas/Navigation/index.tsx
+++ src/Kanbas/Navigation/index.tsx
@@ -1,19 +1,42 @@
 import { Link, useLocation } from "react-router-dom";
-import { FaTachometerAlt, FaRegUserCircle, FaBook, FaRegCalendarAlt, FaInbox, FaHistory, FaStudiovinari, FaExternalLinkSquareAlt, FaArrowAltCircleRight, FaQuestionCircle } from "react-icons/fa";
+import "./index.css";
+import { FaTachometerAlt, FaRegUserCircle, FaBook, FaInbox, FaCalendar, FaClock, FaArrowRight, FaQuestion } from "react-icons/fa";
 function KanbasNavigation() {
   const links = [
+    { label: "N"},
     { label: "Account",   icon: <FaRegUserCircle className="fs-2" />  },
     { label: "Dashboard", icon: <FaTachometerAlt className="fs-2" />  },
     { label: "Courses",   icon: <FaBook className="fs-2" />           },
-    { label: "Calendar",  icon: <FaRegCalendarAlt className="fs-2" /> },
-    { label: "Inbox",    icon: <FaInbox className="fs-2" />},
-    { label: "History",  icon: <FaHistory className="fs-2" />},
-    { label: "Studio",  icon: <FaExternalLinkSquareAlt className="fs-2" />},
-    { label: "Commons",  icon: <FaArrowAltCircleRight className="fs-2" />},
-    { label: "Help",  icon: <FaQuestionCircle className="fs-2" />}
+    { label: "Calendar",  icon: <FaCalendar className="fs-2" /> },
+    { label: "Inbox", icon: <FaInbox className="fs-2" />},
+    { label: "History", icon: <FaClock className="fs-2" />},
+    { label: "Studio", icon: <FaCalendar className="fs-2" />},
+    { label: "Commons", icon: <FaArrowRight className="fs-2" />},
+    { label: "Help", icon: <FaQuestion className="fs-2" />},
   ];
   const { pathname } = useLocation();
   return (
+
+
+
+//     <ul className="wd-kanbas-navigation">
+//   {pathname.includes("Courses") ? (
+//     <li className={pathname.includes("Dashboard") ? "wd-active" : ""}>
+//       <Link to="/Kanbas/Dashboard"> Dashboard </Link>
+//     </li>
+//   ) : (
+//     links.map((link, index) => (
+//       <li key={index} className={pathname.includes(link.label) ? "wd-active" : ""}>
+//         <Link to={`/Kanbas/${link.label}`}> {link.icon} {link.label} </Link>
+//       </li>
+//     ))
+//   )}
+// </ul>
+
+
+
+
+
     <ul className="wd-kanbas-navigation">
       {links.map((link, index) => (
         <li key={index} className={pathname.includes(link.label) ? "wd-active" : ""}>
