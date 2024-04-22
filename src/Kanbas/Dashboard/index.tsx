ecd11d9bb24b1d708a32149522979c3a6e0a183c
David-JDixon <98180004+David-JDixon@users.noreply.github.com>
2024-04-21 20:31:51 -0400

    Add files via upload

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,79 +1,90 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import * as db from "../Database";
 
-function Dashboard({ courses, course, setCourse, addNewCourse,
-  deleteCourse, updateCourse }: {
-  courses: any[]; course: any; setCourse: (course: any) => void;
-  addNewCourse: () => void; deleteCourse: (course: any) => void;
-  updateCourse: () => void; })
-  {
+function Dashboard(
+  { courses, course, setCourse, addNewCourse,
+    deleteCourse, updateCourse }: {
+    courses: any[]; course: any; setCourse: (course: any) => void;
+    addNewCourse: () => void; deleteCourse: (course: any) => void;
+    updateCourse: () => void; })
+   {
+  // const [courses, setCourses] = useState(db.courses);
+  // const [course, setCourse] = useState({
+  //   _id: "0", name: "New Course", number: "New Number",
+  //   startDate: "2023-09-10", endDate: "2023-12-15",
+  //   image: "/images/reactjs.jpg"
+  // });
+  // const addNewCourse = () => {
+  //   const newCourse = { ...course,
+  //                       _id: new Date().getTime().toString() };
+  //   setCourses([...courses, { ...course, ...newCourse }]);
+  // };
+  // const deleteCourse = (courseId: string) => {
+  //   setCourses(courses.filter((course) => course._id !== courseId));
+  // };
+  // const updateCourse = () => {
+  //   setCourses(
+  //     courses.map((c) => {
+  //       if (c._id === course._id) {
+  //         return course;
+  //       } else {
+  //         return c;
+  //       }
+  //     })
+  //   );
+  // };
+
+
   return (
     <div className="p-4">
-      <h1>Dashboard</h1> <hr />
+      <h1>Dashboard</h1>              <hr />
       <h5>Course</h5>
-      <input
-        value={course.name}
-        className="form-control"
-        onChange={(e) => setCourse({ ...course, name: e.target.value })}
-      />
-      <input
-        value={course.number}
-        className="form-control"
-        onChange={(e) => setCourse({ ...course, number: e.target.value })}
-      />
-      <input
-        value={course.startDate}
-        className="form-control"
-        type="date"
-        onChange={(e) => setCourse({ ...course, number: e.target.value })}
-      />
-      <input
-        value={course.endDate}
-        className="form-control"
-        type="date"
-        onChange={(e) => setCourse({ ...course, number: e.target.value })}
-      />
-      <button onClick={addNewCourse}>Add</button>
-      <button onClick={updateCourse}>Update</button>
-      <h2>Published Courses ({courses.length})</h2> <hr />
+      <input value={course.name} className="form-control"
+             onChange={(e) => setCourse({ ...course, name: e.target.value }) } />
+      <input value={course.number} className="form-control"
+             onChange={(e) => setCourse({ ...course, number: e.target.value }) } />
+      <input value={course.startDate} className="form-control" type="date"
+             onChange={(e) => setCourse({ ...course, startDate: e.target.value }) }/>
+      <input value={course.endDate} className="form-control" type="date"
+             onChange={(e) => setCourse({ ...course, endDate: e.target.value }) } />
+      <button onClick={updateCourse} >
+        Update
+      </button>
+      <button onClick={addNewCourse} >
+        Add
+      </button>
+
+      <h2>Published Courses (12)</h2> <hr />
       <div className="row">
         <div className="row row-cols-1 row-cols-md-5 g-4">
           {courses.map((course) => (
-            <div key={course._id} className="col" style={{ width: "300px" }}>
+            <div key={course._id} className="col" style={{ width: 300 }}>
               <div className="card">
-                <img src="https://wallpapercave.com/wp/7Zt88LO.jpg" className="card-img-top" alt="..." />
-                <Link className="card-title" to={`/Kanbas/Courses/${course._id}/Home`}
+                <img src={`/images/${course.image}`} className="card-img-top"
+                     style={{ height: 150 }}/>
+                <div className="card-body">
+                  <Link className="card-title" to={`/Kanbas/Courses/${course._id}/Home`}
                     style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
-                  {course.name}
-                  <div key={course._id} className="card-text">
-                    {course._id}
-                  </div>
-                  <p className="card-desc"> Spring24 Semester Term</p>
-                  <br/>
-                  <div className="wd-icon"/>
-                  <Link to={`/Kanbas/Courses/${course._id}/Home`} className="btn btn-primary">
-                    Go </Link> 
-                  <button
-                    onClick={(event) => {
+                    {course.name}
+                    <button onClick={(event) => {
                       event.preventDefault();
                       setCourse(course);
-                    }}
-                  >
+                    }}>
                     Edit
-                  </button>
-
-                  <button
-                    onClick={(event) => {
-                      event.preventDefault();
-                      deleteCourse(course._id);
-                    }}
-                  >
-                    Delete
-                  </button>
-
-                </Link>
-
-              </div> 
+                    </button>
+                    <button onClick={(event) => {
+                        event.preventDefault();
+                        deleteCourse(course._id);
+                      }}>
+                      Delete
+              </button>
+</Link>
+                  <p className="card-text">{course.name}</p>
+                  <Link to={`/Kanbas/Courses/${course._id}/Home`} className="btn btn-primary">
+                    Go </Link>
+                </div>
+              </div>
             </div>
           ))}
         </div>
@@ -82,3 +93,7 @@ function Dashboard({ courses, course, setCourse, addNewCourse,
   );
 }
 export default Dashboard;
+
+
+
+
