9c6d92e4c51e0bdcacb597b55fdac369d9a1e2bc
David-JDixon <98180004+David-JDixon@users.noreply.github.com>
2024-04-21 20:27:38 -0400

    Add files via upload

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -1,53 +1,46 @@
 import React from "react";
 import { FaCheckCircle, FaEllipsisV, FaPlusCircle } from "react-icons/fa";
 import { Link, useParams } from "react-router-dom";
-import { assignments, courses, } from "../../Database";
-
+import { assignments } from "../../Database";
+import "./index.css";
 function Assignments() {
-    const { courseId } = useParams();
-    const assignmentList = assignments.filter(
-        (assignment) => assignment.course === courseId
-    );
-
-    return (
-        <> <div><h2>Assignments</h2></div>
-            <div className="flex-grow-0 me-2 d-none d-lg-block">
-                <input type="text" placeholder="Search for Assignments" />
-                <button className="btn btn-secondary">+Group <i className="fa fa-times"></i></button>
-                <button className="btn btn-fail">...Assignment <i className="fa fa-check"></i></button>
-            </div>
-            <ul className="list-group wd-modules">
-                <li className="list-group-item">
-                    <div>
-                        <FaEllipsisV className="me-2" /> ASSIGNMENTS
-                        <span className="float-end">
-                            <FaCheckCircle className="text-success" />
-                            <FaPlusCircle className="ms-2" />
-                            <FaEllipsisV className="ms-2" />
-                        </span>
-                        (40% of Total)
-                    </div>
-                    <ul className="list-group">
-                        {assignmentList.map((assignment) => (
-                            <>
-                                <li className="list-group-item">
-                                    <FaEllipsisV className="me-2" />
-                                    <Link to={`/Kanbas/Courses/${courseId}/Assignments/${assignment._id}`}>
-                                        {assignment.title}
-                                    </Link>
-                                    <span className="float-end">
-                                        <FaCheckCircle className="text-success" />
-                                        <FaEllipsisV className="ms-2" />
-                                    </span>
-                                </li>
-                                <div className="list-group-item" style={{ color: "orange" }}>Multiple Modules | Due: {assignment._id}</div>
-                            </>
-                        ))}
-                    </ul>
-                </li>
-            </ul>
-        </>
-    );
-}
-
+  const { courseId } = useParams();
+  const assignmentList = assignments.filter(
+    (assignment) => assignment.course === courseId);
+  return (
+    <>
+      <div>
+        <label htmlFor="textBox"></label>
+        <input
+          type="text"
+          id="textBox"
+          value="Search for assignment"
+        />
+      
+        <button>+ Group</button>
+        <button className="redButton">+ Assignment</button>
+      </div>
+      <ul className="list-group wd-modules">
+        <li className="list-group-item">
+          <div>
+            <FaEllipsisV className="me-2" /> ASSIGNMENTS
+            <span className="float-end">
+              <FaCheckCircle className="text-success" />
+              <FaPlusCircle className="ms-2" /><FaEllipsisV className="ms-2" />
+            </span>
+          </div>
+          <ul className="list-group">
+            {assignmentList.map((assignment) => (
+              <li className="list-group-item">
+                <FaEllipsisV className="me-2" />
+                <Link
+                   to={`/Kanbas/Courses/${courseId}/Assignments/${assignment._id}`}>{assignment.title}</Link>
+                <span className="float-end">
+                  <FaCheckCircle className="text-success" /><FaEllipsisV className="ms-2" /></span>
+              </li>))}
+          </ul>
+        </li>
+      </ul>
+    </>
+);}
 export default Assignments;
