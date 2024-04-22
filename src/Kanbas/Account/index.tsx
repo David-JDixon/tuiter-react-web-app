d2232c5a0e0db13d65d127f4debf426aacf0216f
David-JDixon <98180004+David-JDixon@users.noreply.github.com>
2024-04-21 20:26:50 -0400

    Add files via upload

--- src/Kanbas/Account/index.tsx
+++ src/Kanbas/Account/index.tsx
@@ -1,13 +1,13 @@
-import UserTable from "../../Users/Table";
 import Signin from "../../Users/Signin";
 import Profile from "../../Users/Profile";
+import UserTable from "../../Users/Table";
 import { Routes, Route, Navigate } from "react-router-dom";
 export default function Account() {
   return (
     <div className="container-fluid">
       <Routes>
-        <Route path="/Admin/Users" element={<UserTable />} />
         <Route path="/" element={<Navigate to="/Kanbas/Account/Signin" />} />
+        <Route path="/Admin/Users" element={<UserTable />} />
         <Route path="/Signin" element={<Signin />} />
         <Route path="/Profile" element={<Profile />} />
       </Routes>
