6c99794f58aa104d4f22a96be2e96c465fd4560e
David-JDixon <98180004+David-JDixon@users.noreply.github.com>
2024-04-26 17:56:16 -0400

    Delete Downloads/tuiter-react-web-app-a4/tuiter-react-web-app-a4 directory

--- Downloads/tuiter-react-web-app-a4/tuiter-react-web-app-a4/src/Labs/a3/JavaScript/functions/ImpliedReturn.tsx
+++
@@ -1,7 +0,0 @@
-function impliedReturn () {
-    const multiply = (a: number, b: number) => a * b;
-    const fourTimesFive = multiply(4, 5);
-    console.log(fourTimesFive);
-}
-
-export default impliedReturn;
