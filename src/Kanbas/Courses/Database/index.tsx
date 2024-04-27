400f9d7c844c782d044e93cde1774a27ad5026a3
David-JDixon <98180004+David-JDixon@users.noreply.github.com>
2024-04-26 20:25:10 -0400

    Add files via upload

--- src/Kanbas/Courses/Database/index.tsx
+++ src/Kanbas/Courses/Database/index.tsx
@@ -1,1 +1,5 @@
-
+import courses from "./courses.json";
+import modules from "./modules";
+import assignments from "./assignments";
+import todos from "./todos.json";
+export default { courses, modules, assignments, todos };
