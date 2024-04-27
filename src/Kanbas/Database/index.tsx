b037ab016b58389aaa28a9765c0a26f0903028a7
David-JDixon <98180004+David-JDixon@users.noreply.github.com>
2024-04-26 20:24:14 -0400

    Delete src/Kanbas/Database directory

--- src/Kanbas/Database/index.tsx
+++
@@ -1,5 +0,0 @@
-import courses from "./courses.json";
-import modules from "./modules";
-import assignments from "./assignments";
-import todos from "./todos.json";
-export default { courses, modules, assignments, todos };
